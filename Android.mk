9770b7e548576921c965ce3bf78cda59ca475689
Atroc X <93807457+Atroc-X@users.noreply.github.com>
2023-11-14 10:24:49 +0800

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -3,4 +3,8 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE    := your_script
 LOCAL_SRC_FILES := your_script.cpp
+LOCAL_LDLIBS    := -llog
+LOCAL_CPPFLAGS  := -std=c++11
+LOCAL_CPP_FEATURES := rtti exceptions
+LOCAL_STATIC_LIBRARIES := c++_static
 include $(BUILD_EXECUTABLE)
