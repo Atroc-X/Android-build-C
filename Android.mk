aae5c02ad1295726a98973ed47f1007beb9599e4
Atroc X <93807457+Atroc-X@users.noreply.github.com>
2023-11-14 10:29:26 +0800

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -4,6 +4,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := your_script
 LOCAL_SRC_FILES := your_script.cpp
 LOCAL_LDLIBS    := -llog
-LOCAL_CPPFLAGS  := -std=c++11 -frtti -fexceptions
-LOCAL_C_INCLUDES := $(NDK_ROOT)/sources/cxx-stl/llvm-libc++/include
+LOCAL_CPPFLAGS  := -std=c++11
+LOCAL_CPP_FEATURES := rtti exceptions
 include $(BUILD_EXECUTABLE)
