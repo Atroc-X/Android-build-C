97cf551d6633635b09df539941a47568a7bd5c67
Atroc X <93807457+Atroc-X@users.noreply.github.com>
2023-11-14 15:59:50 +0800

    Update your_script.cpp

--- your_script.cpp
+++ your_script.cpp
@@ -1,91 +1,9 @@
 #include <iostream>
-#include <fstream>
-#include <string>
 #include <vector>
+#include <string>
 #include <cstdlib>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <sys/types.h>
-#include <dirent.h>
-#include <cstring>
 
-std::string exec(const char* cmd) {
-    char buffer[128];
-    std::string result = "";
-    FILE* pipe = popen(cmd, "r");
-    if (!pipe) {
-        std::cerr << "popen() failed!" << std::endl;
-        return result;
-    }
-
-    while (fgets(buffer, sizeof buffer, pipe) != NULL) {
-        result += buffer;
-    }
-
-    pclose(pipe);
-    return result;
-}
-
-void startProxy() {
-    pid_t pid = fork();
-    if (pid == 0) {
-        execl("/bin/sh", "sh", "-c", "/data/Vinnet/core/redsocks2 -c /data/Vinnet/core/redsocks.conf", nullptr);
-        exit(EXIT_FAILURE);
-    } else if (pid < 0) {
-        std::cerr << "Failed to fork" << std::endl;
-    }
-}
-
-void handleSgameStart() {
-    if (exec("netstat -tuln | grep -q \"127.0.0.1:55555\"").empty()) {
-        startProxy();
-    }
-}
-
-bool ruleExists(const std::string& rule) {
-    std::string command = "iptables -t nat -C " + rule + " >/dev/null 2>&1";
-    return system(command.c_str()) == 0;
-}
-
-void addIptablesRules() {
-    std::vector<std::string> rules = {
-        "-t nat -A OUTPUT -d 119.147.15.56 -p tcp --dport 443 -j DNAT --to-destination 127.0.0.1:55555",
-        "-t nat -A OUTPUT -d 157.255.209.79 -p tcp --dport 443 -j DNAT --to-destination 127.0.0.1:55555",
-        "-t nat -A OUTPUT -d 112.53.47.25 -p tcp --dport 443 -j DNAT --to-destination 127.0.0.1:55555",
-        "-t nat -A OUTPUT -d 43.136.1.72 -p tcp --dport 443 -j DNAT --to-destination 127.0.0.1:55555"
-    };
-
-    for (const auto& rule : rules) {
-        if (!ruleExists(rule)) {
-            std::string command = "iptables " + rule;
-            system(command.c_str());
-        }
-    }
-}
-
-std::string findSgameDirectory() {
-    std::string systemPath = "/private/var/mobile/Containers/Data/Application";
-    DIR* dir = opendir(systemPath.c_str());
-    struct dirent* entry;
-
-    if (dir == nullptr) {
-        std::cerr << "Failed to open directory: " << systemPath << std::endl;
-        return "";
-    }
-
-    while ((entry = readdir(dir)) != nullptr) {
-        if (entry->d_type == DT_DIR) {
-            std::string subDirPath = systemPath + "/" + entry->d_name + "/Documents/ShadowTrackerExtra";
-            if (access(subDirPath.c_str(), F_OK) != -1) {
-                closedir(dir);
-                return systemPath + "/" + entry->d_name; // Found the game directory
-            }
-        }
-    }
-
-    closedir(dir);
-    return ""; // Game directory not found
-}
+// ... 其他代码部分保持不变 ...
 
 void deleteSgamePrefFiles(const std::string& appPath) {
     std::vector<std::string> pathsToDelete = {
@@ -116,37 +34,9 @@ void deleteSgamePrefFiles(const std::string& appPath) {
 
     for (const auto& path : pathsToDelete) {
         std::string fullPath = appPath + path;
-        std::string command = "rm -rf " + fullPath;
+        std::string command = "rm -rf " + fullPath + " && echo 'Deleted: " + fullPath + "' || echo 'Failed to delete: " + fullPath + "'";
         system(command.c_str());
     }
 }
 
-void startService() {
-    const std::string procName = "com.tencent.tmgp.sgame";
-
-    while (true) {
-        if (exec(("ps -A | grep " + procName + " | wc -l").c_str()) != "0\n") {
-            handleSgameStart();
-
-            while (exec(("ps -A | grep " + procName + " | wc -l").c_str()) != "0\n") {
-                sleep(5);
-            }
-
-            std::string appPath = findSgameDirectory();
-            if (!appPath.empty()) {
-                deleteSgamePrefFiles(appPath);
-            }
-        }
-
-        sleep(5);
-    }
-}
-
-int main() {
-    std::cout << "开始运行" << std::endl;
-
-    addIptablesRules();
-    startService();
-
-    return 0;
-}
+// ... 其他代码部分保持不变 ...
