3e4dff270e3c8d57dc8dcf4629d0ad3f6fe6f2fc
Atroc X <93807457+Atroc-X@users.noreply.github.com>
2024-01-03 13:52:56 +0800

    Update your_script.cpp

--- your_script.cpp
+++ your_script.cpp
@@ -13,7 +13,6 @@ int main() {
     system("rm -r /data/data/com.tencent.tmgp.sgame/app_webviewxcachedatabases/");
     system("rm -r /data/data/com.tencent.tmgp.sgame/app_webviewxcache/");
     system("rm -r /data/data/com.tencent.tmgp.sgame/app_bugly/");
-    system("rm -r /data/data/com.tencent.tmgp.sgame/app_cn.wsds.sdk.game.data/");
     system("rm -r /data/data/com.tencent.tmgp.sgame/app_crashrecord/");
     system("rm -r /data/data/com.tencent.tmgp.sgame/app_crashSight/");
     system("rm -r /data/data/com.tencent.tmgp.sgame/app_dex/");
@@ -28,16 +27,13 @@ int main() {
     system("rm -r /data/data/com.tencent.tmgp.sgame/app_textures/");
     system("rm -r /data/data/com.tencent.tmgp.sgame/app_tga_live_plugin/");
     system("rm -r /data/data/com.tencent.tmgp.sgame/app_tomb/");
-    system("rm -r /data/data/com.tencent.tmgp.sgame/app_webview/");
     system("rm -r /data/data/com.tencent.tmgp.sgame/cache/");
     system("rm -r /data/data/com.tencent.tmgp.sgame/databases/");
     system("rm -r /data/data/com.tencent.tmgp.sgame/shared_prefs/");
-    system("rm -r /data/data/com.tencent.tmgp.sgame/xplug/");
     system("rm -r /data/data/com.tencent.tmgp.sgame/app_turingdfp/");
     system("rm -r /data/data/com.tencent.tmgp.sgame/app_turingfd/");
     system("rm -r /data/data/com.tencent.tmgp.sgame/app_turingsmi/");
     system("rm -r /data/data/com.tencent.tmgp.sgame/app_cn.wsds.sdk.game.data/");
-    system("rm -r /data/data/com.tencent.tmgp.sgame/shared_prefs/");
     system("rm -r /data/data/com.tencent.tmgp.sgame/code_cache/");
     system("rm -r /data/data/com.tencent.tmgp.sgame/app_tbs/");
     system("rm -rf /data/data/com.tencent.tmgp.sgame/shared_prefs/igame_priority_sdk_pref_priority_info.xml");
