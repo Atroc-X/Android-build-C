1060bdc8e8102d331f241d6da9bcf6a7a4487ac5
Atroc X <93807457+Atroc-X@users.noreply.github.com>
2024-01-03 13:35:36 +0800

    Update your_script.cpp

--- your_script.cpp
+++ your_script.cpp
@@ -11,11 +11,25 @@ int main() {
     system("rm -rf /data/data/com.tencent.tmgp.sgame/files/ano_tmp");
     system("rm -rf /data/user/0/com.tencent.tmgp.sgame/files/ano_tmp");
 
+    // 执行Shell脚本
+    system("echo");
+    system("prog_name=\"/data/temp\"");
+    system("name=$(tr -dc '1-9' < /dev/urandom | head -c 8)");
+    system("while echo \"$name\" | grep -q '\''; do name=$(tr -dc '1-9' < /dev/urandom | head -c 8); done");
+    system("yy=$(getprop ro.serialno)");
+    system("echo \"原值：ro.serialno:$yy\"");
+    system("resetprop ro.serialno $name");
+    system("echo");
+    system("yy=$(getprop ro.serialno)");
+    system("echo \"修改后：ro.serialno:$yy\"");
+    system("echo");
+    system("echo \"因为重启恢复的缘故，所以每次重启都要执行一遍该脚本。\"");
+
     std::cout << "------------\n";
     std::cout << "Vinnet提示：\n";
     std::cout << "执行成功!\n";
     std::cout << "校验残留清理完成\n";
     std::cout << "------------\n";
-
+    
     return 0;
 }
